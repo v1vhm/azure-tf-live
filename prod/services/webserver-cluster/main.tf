2a180f7e4f365d3d24a46af9a3340f19e62aa89a
Matt Buchanan <matt@v1vhm.onmicrosoft.com>
2023-09-23 13:05:25 +0100

    updating main to use github module

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -26,7 +26,7 @@ terraform {
 }
 
 module "webserver_cluster" {
-  source                          = "../../../modules/services/webserver-cluster"
+  source                          = "git@github.com:mattv1vhm/azure-tf-modules.git//services/webserver-cluster?ref=v0.0.1"
   cluster_name                    = "webserver"
   environment                     = "prod"
   db_remote_state_key             = "prod/data-stores/mysql/terraform.tfstate"
